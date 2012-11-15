3f08bafceccd4de02c02073356518708e72a7354
22jayferd <22jayferd@gmail.com>
2012-11-14 19:12:19 -0500

    Necessary configuration changes

--- src/main/java/viewer/security/authentication/UsernameLookupUserDetailsServiceImpl.java
+++ src/main/java/viewer/security/authentication/UsernameLookupUserDetailsServiceImpl.java
@@ -5,26 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
 import viewer.dao.UserDao;
 import viewer.model.User;
 
 @Service("usernameUserDetailsService")
 public class UsernameLookupUserDetailsServiceImpl extends AbstractUserDetailsService {
 
-	@Autowired
-	private UserDao userDao;
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    @Autowired
+    private UserDao userDao;
 
-		if (StringUtils.isBlank(username)) {
-			throw new UsernameNotFoundException("Username not found: Username is blank");
-		}
-		User user = userDao.readByUsernameForPasswordLogin(username);
-		if (user == null) {
-			throw new UsernameNotFoundException("Username not found: " + username);
-		}
-		return createUserDetails(user);
-	}
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        if (StringUtils.isBlank(username)) {
+            throw new UsernameNotFoundException("Username not found: Username is blank");
+        }
+        User user = userDao.readByUsernameForPasswordLogin(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username not found: " + username);
+        }
+        return createUserDetails(user);
+    }
 
 }
